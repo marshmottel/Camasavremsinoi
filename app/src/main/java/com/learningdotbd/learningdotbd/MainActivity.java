49cfbf73634bf669c7071f70ef8274002d0a6e26
Marshmottel <ob133@student.ugal.ro>
2023-06-29 12:24:45 +0300

    dsgfgnyh

---
+++ app/src/main/java/com/learningdotbd/learningdotbd/MainActivity.java
@@ -0,0 +1,84 @@
+package com.learningdotbd.learningdotbd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+
+        load();
+        setDesktopMode(webView, true);
+    }
+
+
+
+    public void load() {
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setDomStorageEnabled(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+        });
+        webView.setWebViewClient(new Callback());
+        webView.loadUrl("file:///android_asset/index.html");
+       // webView.loadUrl("https://google.com/");
+
+
+    }
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return false;
+        }
+    }
+    public void setDesktopMode( WebView webView,boolean enabled) {
+        String newUserAgent = webView.getSettings().getUserAgentString();
+        if (enabled) {
+            try {
+                String ua = webView.getSettings().getUserAgentString();
+                String androidOSString = webView.getSettings().getUserAgentString().substring(ua.indexOf("("), ua.indexOf(")") + 1);
+                newUserAgent = webView.getSettings().getUserAgentString().replace(androidOSString, "(X11; Linux x86_64)");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            newUserAgent = null;
+        }
+
+        webView.getSettings().setUserAgentString(newUserAgent);
+        webView.getSettings().setUseWideViewPort(enabled);
+        webView.getSettings().setLoadWithOverviewMode(enabled);
+        webView.reload();
+    }
+    @Override
+    public void onBackPressed() {
+        if(webView !=null && webView.canGoBack()){
+            webView.goBack();
+        }else {
+        super.onBackPressed();
+        }
+    }
+}
